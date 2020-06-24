b3e8a7751b84df5db1bb598a54bf52ed07f088a4
brookeperkins <brookemmperkins@gmail.com>
2020-06-24 16:37:32 -0700

    nametag features

---
+++ app.js
@@ -0,0 +1,27 @@
+const pinkButton = document.getElementById('pink');
+
+pinkButton.addEventListener('click', () => {
+console.log("pink button here")
+ //need to get the div we want to change
+ //change background color of div with document/getelementbyIS()
+ //must assign id to each div in order to access
+
+ //get input
+ //get inputs value
+ //get div
+ //set text context of div to input value
+})
+
+const pinkButton = document.getElementById('blue');
+
+pinkButton.addEventListener('click', () => {
+console.log("blue button here")
+
+})
+
+const pinkButton = document.getElementById('green');
+
+pinkButton.addEventListener('click', () => {
+console.log("green button here")
+
+})

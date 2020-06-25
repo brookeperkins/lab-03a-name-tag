38e7f5f6e347412f8cc0f88367d7599e85ce86af
brookeperkins <brookemmperkins@gmail.com>
2020-06-24 23:27:22 -0700

    first attempts at adding events

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const pinkButton = document.getElementById('pink');
-
+const pinkButton = document.getElementById('pink-button');
 pinkButton.addEventListener('click', () => {
-console.log("pink button here")
+    const nametag=document.getElementById('nametag');
+    const nametag.style.background= 'pink'
+}
+
+const blueButton = document.getElementById('blue-button');
+blueButton.addEventListener('click', () => {
+    const nametag=document.getElementById('nametag');
+    const nametag.style.background= 'blue'
+}
  //need to get the div we want to change
  //change background color of div with document/getelementbyIS()
  //must assign id to each div in order to access
@@ -10,18 +17,24 @@ console.log("pink button here")
  //get inputs value
  //get div
  //set text context of div to input value
-})
+ //make a variable that means the white space where the name should go
+ //then make variable.textcontent= that variable (has to be value of 
+ //that input field) 
 
-const pinkButton = document.getElementById('blue');
-
-pinkButton.addEventListener('click', () => {
-console.log("blue button here")
-
-})
-
-const pinkButton = document.getElementById('green');
-
-pinkButton.addEventListener('click', () => {
-console.log("green button here")
+ //const userNameInput = document.getElementByID('nameField')
 
 })
+
+//const pinkButton = document.getElementById('blue');
+
+//pinkButton.addEventListener('click', () => {
+//console.log("blue button here")
+
+//})
+
+//const pinkButton = document.getElementById('green');
+
+//pinkButton.addEventListener('click', () => {
+//console.log("green button here")
+
+//})

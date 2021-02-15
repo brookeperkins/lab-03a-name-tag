db16c184d0b1c200b3f1e5ff7063923a07a30515
brookeperkins <brookemmperkins@gmail.com>
2021-02-15 10:58:27 -0800

    lolllll just for funsies why not

--- app.js
+++ app.js
@@ -1,40 +1,26 @@
 const pinkButton = document.getElementById('pink-button');
-pinkButton.addEventListener('click', () => {
-    const nametag=document.getElementById('nametag');
-    const nametag.style.background= 'pink'
-}
-
 const blueButton = document.getElementById('blue-button');
+const greenButton = document.getElementById('green-button');
+const userButton = document.getElementById('user-button');
+const userInput = document.getElementById('name-input');
+const nameTag = document.getElementById('name-here');
+
+pinkButton.addEventListener('click', () => {
+    const nametag = document.getElementById('nametag');
+    nametag.style.background= 'pink';
+});
+
 blueButton.addEventListener('click', () => {
-    const nametag=document.getElementById('nametag');
-    const nametag.style.background= 'blue'
-}
- //need to get the div we want to change
- //change background color of div with document/getelementbyIS()
- //must assign id to each div in order to access
-
- //get input
- //get inputs value
- //get div
- //set text context of div to input value
- //make a variable that means the white space where the name should go
- //then make variable.textcontent= that variable (has to be value of 
- //that input field) 
-
- //const userNameInput = document.getElementByID('nameField')
-
+    const nametag = document.getElementById('nametag');
+    nametag.style.background= 'steelblue';
 })
 
-//const pinkButton = document.getElementById('blue');
+greenButton.addEventListener('click', () => {
+    const nametag = document.getElementById('nametag');
+    nametag.style.background = 'green';
+})
 
-//pinkButton.addEventListener('click', () => {
-//console.log("blue button here")
-
-//})
-
-//const pinkButton = document.getElementById('green');
-
-//pinkButton.addEventListener('click', () => {
-//console.log("green button here")
-
-//})
+userButton.addEventListener('click', () => {
+    const newInput = userInput.value;
+    nameTag.textContent = newInput;
+})
